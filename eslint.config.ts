8ed907225c3249103317b2c189db7c315f1c8906
Jaid <6216144+Jaid@users.noreply.github.com>
2026-08-31 04:15:32 +0100

    chore: Normalized ESLint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,12 +1,3 @@
-import type {Linter} from 'eslint'
-
 import {makeEslintConfig} from 'eslint-config-jaid'
 
-const eslintConfig: Array<Linter.Config> = [
-  {
-    ignores: ['dist/**', 'private/**', 'temp/**'],
-  },
-  ...makeEslintConfig(),
-]
-
-export default eslintConfig
+export default makeEslintConfig()
